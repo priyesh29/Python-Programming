a245cd5854d670288aecfa926ee9b2f7b66e13f0
Priyesh Kumar <47806603+priyesh29@users.noreply.github.com>
2023-03-21 17:16:06 +0530

    Add files via upload

---
+++ ComparisonOperators.py
@@ -0,0 +1,12 @@
+#Comparison Operator compares two variables and return a boolean result True or False
+
+#declare two variables a and b 
+a = 10
+b = 5
+
+print('a == b =', a ==b)
+print('\na != b =', a !=b)
+print('\na > b =', a > b)
+print('\na < b =', a < b)
+print('\na >= b =', a >= b)
+print('\na <= b =', a <= b)
