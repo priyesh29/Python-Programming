748d04ee851d0ccbbc8ff1ff8fd70672c282a9fb
Priyesh Kumar <47806603+priyesh29@users.noreply.github.com>
2023-03-29 21:14:32 +0530

    Add files via upload

---
+++ BasicProgramming6.py
@@ -0,0 +1,25 @@
+#Arithmetic Operators 
+
+#Operators are special symbols that perform special operations on variables and values.
+#Arithmetic operators includes addition, multiplication, subtraction, division, floor division, modulo, power
+
+#2 variables are taken a and b and assigned random values respectively.
+a = 10
+b = 5
+
+print("Sum : ", a+b)
+print(type(a+b))
+print("\nSubtraction : ", a-b)
+print(type(a-b))
+print("\nMultiplication : ", a*b)
+print(type(a*b))
+print("\nDivision : ", a/b)
+print(type(a/b))
+print("\nModulo : ", a%b)
+print(type(a%b))
+print("\nFloor Division : ", a//b)
+print(type(a//b))
+print("\nPower : ", a**b)
+print(type(a**b))
+
+
