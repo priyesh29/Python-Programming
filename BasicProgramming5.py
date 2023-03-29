930269bbae855f4400de2047076660d9817fa0b9
Priyesh Kumar <47806603+priyesh29@users.noreply.github.com>
2023-03-29 20:54:00 +0530

    Update BasicProgramming5.py

--- BasicProgramming5.py
+++ BasicProgramming5.py
@@ -16,8 +16,4 @@ integer_number = 24
 floating_number = 12.6
 new_number = integer_number + floating_number
 print("The New Number is : ", new_number)
-print(type(new_number))
-
-
-
-
+print(type(new_number)
